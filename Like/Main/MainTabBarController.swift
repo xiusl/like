9b6251e51dc9f091255cba77c8a2daca78295e62
xiusl <xiushilin@hotmail.com>
2019-11-15 15:13:57 +0800

    view: fix bug

--- Like/Main/MainTabBarController.swift
+++ Like/Main/MainTabBarController.swift
@@ -13,25 +13,40 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-    let homeVc = HomeTabViewController()
-            self.setupChildVc(vc: homeVc,
-                              title: "首页",
-                              imageName: "tabbar_home_nor",
-                              selectedImageName: "tabbar_home_sel")
-            
-            let orderVc = DiscoverTabViewController()
-            self.setupChildVc(vc: orderVc,
-                              title: "发现",
-                              imageName: "tabbar_discover_nor",
-                              selectedImageName: "tabbar_discover_sel")
-            
-            let meVc = MeTabViewController()
-            self.setupChildVc(vc: meVc,
-                              title: "我的",
-                              imageName: "tabbar_me_nor",
-                              selectedImageName: "tabbar_me_sel")
-        }
+        let homeVc = HomeTabViewController()
+        self.setupChildVc(vc: homeVc,
+                          title: "首页",
+                          imageName: "tabbar_home_nor",
+                          selectedImageName: "tabbar_home_sel")
         
+        let orderVc = DiscoverTabViewController()
+        self.setupChildVc(vc: orderVc,
+                          title: "发现",
+                          imageName: "tabbar_discover_nor",
+                          selectedImageName: "tabbar_discover_sel")
+        
+        let meVc = MeTabViewController()
+        self.setupChildVc(vc: meVc,
+                          title: "我的",
+                          imageName: "tabbar_me_nor",
+                          selectedImageName: "tabbar_me_sel")
+    
+        let font = UIFont.systemFont(ofSize: 10, weight: .medium)
+        let color = UIColor.init(hex: 0xB1B1B1)
+        let sColor = UIColor.init(hex: 0x44C7FB)
+        if #available(iOS 13.0, *) {
+            self.tabBar.tintColor = .red
+            self.tabBar.unselectedItemTintColor = color
+            let item = UITabBarItem.appearance()
+            item.setTitleTextAttributes([.font: font], for: .normal)
+            item.setTitleTextAttributes([.font: font], for: .selected)
+        } else {
+            let item = UITabBarItem.appearance()
+            item.setTitleTextAttributes([.font: font, .foregroundColor: color], for: .normal)
+            item.setTitleTextAttributes([.font: font, .foregroundColor: sColor], for: .selected)
+        }
+    }
+    
         func setupChildVc(vc: UIViewController, title: String, imageName: String, selectedImageName: String) {
             let navVc = MainNavigationController.init(rootViewController: vc)
             vc.title = title
@@ -39,11 +54,8 @@ class MainTabBarController: UITabBarController {
             navVc.tabBarItem.image = UIImage(named: imageName)?.withRenderingMode(.alwaysOriginal)
             navVc.tabBarItem.selectedImage = UIImage(named: selectedImageName)?.withRenderingMode(.alwaysOriginal)
             
-            var font = UIFont.systemFont(ofSize: 10)
-            if #available(iOS 8.2, *) {
-                font = UIFont.systemFont(ofSize: 10, weight: .medium)
-            }
-            let color = UIColor.init(hex: 0x1A2C3F)
+            let font = UIFont.systemFont(ofSize: 10, weight: .medium)
+            let color = UIColor.init(hex: 0xB1B1B1)
             let sColor = UIColor.init(hex: 0x44C7FB)
             let textStyle = [NSAttributedString.Key.font: font, NSAttributedString.Key.foregroundColor:color]
             let sTextStyle = [NSAttributedString.Key.font: font, NSAttributedString.Key.foregroundColor:sColor]
--- Like/Me/MeTabViewController.swift
+++ Like/Me/MeTabViewController.swift
@@ -123,6 +123,9 @@ class MeTabViewController: BaseViewController, UITableViewDataSource, UITableVie
             }
         } else if title == "设置" {
             ConfirmSheetView.showInWindow()
+        } else if title == "我的发布" {
+            let vc = MyPostArticlesViewController()
+            self.navigationController?.pushViewController(vc, animated: true)
         }
     }
     
