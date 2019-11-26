59f495269587e18dc927e9c7dd4597188cd3ecb0
xiusl <xiushilin@hotmail.com>
2019-11-26 11:19:44 +0800

    fix

--- Like/Utils/ApiManager/ApiManager.swift
+++ Like/Utils/ApiManager/ApiManager.swift
@@ -22,8 +22,13 @@ public enum Method: String {
 
 class ApiManager: NSObject {
     static let shared = ApiManager()
-//    let baseUrl = "https://ins-api.sleen.top/"
-    static let baseUrl = "http://127.0.0.1:5000/"
+    static let baseUrl = "https://ins-api.sleen.top/"
+//  static let baseUrl = "http://127.0.0.1:5000/"
+//    #if DEBUG
+//        static let baseUrl = "http://127.0.0.1:5000/"
+//    #else
+//        static let baseUrl = "https://ins-api.sleen.top/"
+//    #endif
     private override init() {
         
     }
@@ -34,7 +39,8 @@ class ApiManager: NSObject {
         
         let delegate: SessionDelegate = SessionDelegate()
         let tr = ServerTrustManager(evaluators: [
-            "up-z2.qiniu.com": DisabledEvaluator()
+            "up-z2.qiniu.com": DisabledEvaluator(),
+            "ins-api.sleen.top": DisabledEvaluator()
         ])
         
         return Session(startRequestsImmediately: true, configuration: config, delegate: delegate, rootQueue: DispatchQueue.main, requestQueue: nil, serializationQueue: nil, adapter: nil, serverTrustManager: tr, retrier: nil, eventMonitors: [])
